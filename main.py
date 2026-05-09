4e6a2b81f21973c4fa0c2f5bcf8ae118c0166371
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-09 14:26:14 +0100

    add main.py to orchestrate full ETL pipeline

---
+++ main.py
@@ -0,0 +1,11 @@
+from ingestion.extract import get_recently_played
+from transformation.transform import transform_tracks
+from loading.load import load_tracks
+from datetime import datetime
+
+if __name__ == "__main__":
+    print(f"Pipeline kicking off - {datetime.now()}!")
+    raw_results = get_recently_played()
+    tracks_to_load = transform_tracks(raw_results)
+    load_tracks(tracks_to_load)
+    print(f"Pipeline completed - {datetime.now()}!")
