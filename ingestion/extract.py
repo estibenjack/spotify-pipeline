24ad92130532490ec6dec3acafbf616eff4ba1ac
Steven <156148848+estibenjack@users.noreply.github.com>
2026-05-09 03:23:30 +0100

    refactor extract.py into get_recently_played function

--- ingestion/extract.py
+++ ingestion/extract.py
@@ -3,23 +3,28 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-# load environment variables from .env
-load_dotenv()
 
-spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
-spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
-spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
+def get_recently_played():
+    # load environment variables from .env
+    load_dotenv()
 
-# authenticate with spotify using OAuth and request recently played scope
-scope = "user-read-recently-played"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
-                     client_secret=spotify_client_secret,
-                     redirect_uri=spotify_redirect_uri,
-                     scope=scope))
+    spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    spotify_redirect_uri = os.getenv("SPOTIFY_REDIRECT_URI")
 
-# fetch 50 most recently played tracks
-results = sp.current_user_recently_played(limit=50)
-# for idx, item in enumerate(results['items']):
-#     track = item['track']
-#     print(idx, track['artists'][0]['name'], " - ", track['name'])
-print(results)
+    # authenticate with spotify using OAuth and request recently played scope
+    scope = "user-read-recently-played"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=spotify_client_id,
+                                                   client_secret=spotify_client_secret,
+                                                   redirect_uri=spotify_redirect_uri,
+                                                   scope=scope))
+
+    # fetch 50 most recently played tracks
+    results = sp.current_user_recently_played(limit=50)
+
+    return results
+
+
+if __name__ == "__main__":
+    results = get_recently_played()
+    print(results)
